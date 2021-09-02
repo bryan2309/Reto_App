abca47c3d2f5786fb8a56ad60526d78fd4a018c2
bryan2309 <89115183+bryan2309@users.noreply.github.com>
2021-09-01 19:50:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import requests
 from flask import Flask
 from bs4 import BeautifulSoup
  
+	#Autor: BRYAN SIMBAÑA
+#entrega final de reto extraer información de yahoo finance y mostrarla a traves de una integración continua con Heroku	y Github Action	
+	
 app = Flask(__name__)
 PORT = 8000
 DEBUG = True
@@ -12,6 +15,7 @@ def not_found(error):
 
 @app.route('/', methods=['GET'])
 def index():
+
 	headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
 	crytoBitcoin='https://finance.yahoo.com/quote/BTC-USD?p=BTC-USD'
 	crytoEthereum ='https://finance.yahoo.com/quote/ETH-USD?p=ETH-USD'
@@ -29,7 +33,7 @@ def index():
 	ethereum=cadenaEthereum.find('span',class_='Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)').text.strip()
 	cardano=cadenaCardano.find('span',class_='Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)').text.strip()
 	binancecoin=cadenaCardano.find('span',class_='Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)').text.strip()
-	return 'Valores de BITCOIN $' + ' ' +bitcoin + ' Valores de ETHEREUM $'+ ' ' +ethereum+' Valores de CARDANO Tesla $'+ ' ' +cardano+' Valores de BINANCE COIN $'+ ' ' +binancecoin+''
+	return 'Valores de BITCOIN: $' + ' ' +bitcoin + ' ' + ' Valores de ETHEREUM $'+ ' ' +ethereum+ ' ' +' Valores de CARDANO $'+ ' ' +cardano+ ' ' +' Valores de BINANCE COIN $'+ ' ' +binancecoin+''
 	
 
 if __name__ == '__main__':
